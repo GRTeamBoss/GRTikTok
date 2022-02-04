3c1f921d90e914a4bbc7a1ac52272a13ff4e7d68
GRTeamBoss <rabbitwow97@gmail.com>
2022-02-05 01:48:51 +0500

    Fixing link conflict

--- core/filter.py
+++ core/filter.py
@@ -4,7 +4,8 @@ import re
 def is_link(message):
     try:
         if message.entities[0].type=="url":
-            re.findall(r"http[s]+://[\w\W.]*.com/[\w\W\d]+", message.text)
+            debug = re.findall(r"http[s]+://[\w\W.]*.com/[\w\W\d\s]+", message.text)[0]
+            print("[*] Debug: ", debug)
             return True
         return False
     except Exception:
--- core/function.py
+++ core/function.py
@@ -1,3 +1,6 @@
+import re, subprocess
+
+from TikTokApi import TikTokApi
 import requests
 
 from core.token import bot
@@ -13,8 +16,33 @@ def usage(message) -> None:
 
 
 def download_video(message) -> None:
-    __file = requests.get(message.text)
+    bot.send_message(message.chat.id, "Download...")
+    try:
+        __mobile_link = re.findall(r"https://vm.tiktok.com/[\w\W\s]+", message.text)[0]
+    except Exception:
+        __usual_link = re.findall(r"https://v[\d]+-webapp.tiktok.com/[\w\W\s\S]+", message.text)[0]
+    finally:
+        if message.text == __mobile_link:
+            __redirect_link = subprocess.check_output(f'curl -s "{__mobile_link}"', stderr=subprocess.STDOUT, shell=True).decode()
+            __URI = re.findall(r'="https://m.tiktok.com/[\w\W\s\S]+"', __redirect_link)[0]
+            __URL = __URI[2:-1]
+            print(__URL)
+            __API = TikTokApi().get_video_by_url(video_url=__URL, custom_verifyFP="verify_a70937655d04f49acd1356a7fceb9049")
+            print("__API", __API)
+            exit()
+            __file_link = re.findall(r'src="https://v[\d]+-webapp.tiktok.com/[\w\W\s\S]+"')[0][5:-1]
+            __file = requests.get(__file_link)
+        elif message.text == __usual_link:
+            exit()
+            __file = requests.get(message.text)
+        else:
+            exit()
+            bot.send_message(message.chat.id, "Invalid URI!")
     if __file.status_code == 200:
-        bot.send_video(message.chat.id, __file.content)
+        bot.send_message(message.chat.id, "Success!")
+        if len(__file.content)==0:
+            bot.send_message(message.chat.id, "Video not finded!")
+        else:
+            bot.send_video(message.chat.id, __file.content)
     else:
         bot.send_message(chat_id=message.chat.id, text=f"Error!\nstatus code: {__file.status_code}")
