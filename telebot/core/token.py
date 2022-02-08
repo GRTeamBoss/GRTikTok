e8fed7b67ae0a0ebd951b33b9ccd0bd91a85989c
user <rabbitwow97@gmail.com>
2022-02-08 21:36:23 +0500

    change

--- telebot/core/token.py
+++ telebot/core/token.py
@@ -1,4 +1,4 @@
-from telebot.async_telebot import AsyncTeleBot
+from telebot import TeleBot
 
 TOKEN = "5293683737:AAFDZVtnkCaqoBhwKzco6jZllFInith3Dy0"
-bot = AsyncTeleBot(TOKEN, parse_mode=None)
+bot = TeleBot(TOKEN, parse_mode=None)
